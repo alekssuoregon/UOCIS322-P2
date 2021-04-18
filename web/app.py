37e9a5cf1a01486ce0f4e41a0c7a83f0287f36aa
Aleksandr Stevens <aleksandrstevens@pop-os.localdomain>
2021-04-18 16:29:02 -0700

    finished with the functionality. Need to add configuration

--- web/app.py
+++ web/app.py
@@ -2,13 +2,39 @@
 John Doe's Flask API.
 """
 
-from flask import Flask
+import config
+import os
+from flask import Flask, render_template, send_from_directory, abort
 
 app = Flask(__name__)
+DOCROOT = ""
 
-@app.route("/")
-def hello():
-    return "UOCIS docker demo!\n"
+@app.route("/<path:path>")
+def serve_file(path):
+    full_path = os.path.join(DOCROOT, path)
+    print(path)
+    if any(forbidden_value in path for forbidden_value in ("~", "..", "//")):
+        abort(403)
+    elif not path.endswith((".html", ".css")):
+        abort(401)
+    elif not os.path.isfile(full_path):
+        abort(404)
+    return send_from_directory(DOCROOT, path)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(403)
+def page_forbidden(e):
+    return render_template("403.html"), 403
+
+def configure_options():
+    global DOCROOT
+    options = config.configuration()
+    DOCROOT = options.DOCROOT
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    configure_options()
+    app.run(debug=True, host='0.0.0.0', port=10000)
